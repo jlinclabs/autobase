ff82ec7eed48682ea0b4d4434324eeaf3cd424c9
Jared Grippe <jared@deadlyicon.com>
2022-03-23 11:56:15 -0700

    WIP

--- examples/p2p-multiwriter/swarm-keypair.js
+++
@@ -1,4 +0,0 @@
-export default {
-  publicKey: Buffer.from("604d03ea2045c1adfcb6adad02d71667e03c27ec846fe4f5c4d912c10464aea0", 'hex'),
-  secretKey: Buffer.from("4e9e145fd2f18c6e5b3d86710972d88a8f5c8d8434f07da7c35b3340728f4ee2604d03ea2045c1adfcb6adad02d71667e03c27ec846fe4f5c4d912c10464aea0", 'hex'),
-}
--- examples/p2p-multiwriter/topic.js
+++
@@ -1,1 +0,0 @@
-export default Buffer.from('604d03ea2045c1adfcb6adad02d71667e03c27ec846fe4f5c4d912c10464aea0', 'hex')
